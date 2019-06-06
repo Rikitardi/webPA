e9ab704c0509928cd8d620112c87a1a8423f1dbc
rikitardi <rt.ardiansyah@gmail.com>
2019-06-06 21:56:56 +0700

    Tambah Bootstrap

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
-	<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Untitled Document</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Monitoring Arm Robot</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
 <body>
     <div id="container">
         <div id="header"></div>
         <div id="menu"></div>
         <div id ="konten">
-            <input type"text" id="input1"> 
-            <canvas id="myCanvas" width="640" height="300" style="border: 1px solid #000000;"></canvas>
+            <canvas id="myCanvas" width="1080" height="720" style="border: 1px solid #000000;"></canvas>
             <script type="text/javascript" src="jquery.js"></script>
             <script type="text/javascript" src="tampil.js"></script>
         </div>
