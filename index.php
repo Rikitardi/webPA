ee7aa1cf3b243efb498077425df800365f78dbb3
rikitardi <rt.ardiansyah@gmail.com>
2019-06-06 22:20:50 +0700

    Revert "Tambah Bootstrap"
    This reverts commit e9ab704c0509928cd8d620112c87a1a8423f1dbc.

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
+	<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Monitoring Arm Robot</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <title>Untitled Document</title>
 </head>
 <body>
     <div id="container">
         <div id="header"></div>
         <div id="menu"></div>
         <div id ="konten">
-            <canvas id="myCanvas" width="1080" height="720" style="border: 1px solid #000000;"></canvas>
+            <input type"text" id="input1"> 
+            <canvas id="myCanvas" width="640" height="300" style="border: 1px solid #000000;"></canvas>
             <script type="text/javascript" src="jquery.js"></script>
             <script type="text/javascript" src="tampil.js"></script>
         </div>
